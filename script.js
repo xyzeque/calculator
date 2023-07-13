fd49581b92f2702a0174328ae26ea89affb49bfa
xyzeque <xyzequel@gmail.com>
2023-07-13 12:58:05 +0800

    Add basic operator functions

---
+++ script.js
@@ -0,0 +1,24 @@
+const add = (a, b) => {
+  const sum = a + b;
+  return sum;
+}
+
+const subtract = (a, b) => {
+  const diff = a - b;
+  return diff;
+}
+
+const multiply = (a, b) => {
+  const product = a * b;
+  return product;
+}
+
+const divide = (a, b) => {
+  const quotient = a / b;
+  return quotient;
+}
+
+console.log(add(6,9));
+console.log(subtract(6,9));
+console.log(multiply(6,9));
+console.log(divide(6,9));
