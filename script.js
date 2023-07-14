d6a5339d9b9d174149baef1c9de5a9df47b766c5
xyzeque <xyzequel@gmail.com>
2023-07-14 11:35:26 +0800

    Add initial operate function and variables

--- script.js
+++ script.js
@@ -1,24 +1,45 @@
-const add = (a, b) => {
-  const sum = a + b;
-  return sum;
+// math functions
+function add(a, b) {
+  return a + b;
 }
 
-const subtract = (a, b) => {
-  const diff = a - b;
-  return diff;
+function subtract(a, b) {
+  return a - b;
 }
 
-const multiply = (a, b) => {
-  const product = a * b;
-  return product;
+function multiply(a, b) {
+  return a * b;
 }
 
-const divide = (a, b) => {
-  const quotient = a / b;
-  return quotient;
+function divide(a, b) {
+  if (b === 0) {
+    return 'Error: Division by zero!';
+  }
+  return a / b;
 }
+
 
 console.log(add(6,9));
 console.log(subtract(6,9));
 console.log(multiply(6,9));
 console.log(divide(6,9));
+
+let operator = '';
+let firstNumber = '';
+let secondNumber = '';
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+    default:
+      return 'Invalid operator';
+  }
+}
+
